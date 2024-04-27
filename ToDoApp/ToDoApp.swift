202e98d1f4648b78551d8f3392bba88d986938d7
Zephyr1015 <kurihana_1015@icloud.com>
2024-04-28 01:15:37 +0900

    Initial Commit

---
+++ ToDoApp/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Vincent on 2024/04/28.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
